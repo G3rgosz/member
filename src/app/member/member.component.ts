b2e504a4cd29d603f724efcb4b05d6b39a68d7b3
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2022-03-31 11:12:29 +0200

    dolgozat kész

---
+++ src/app/member/member.component.ts
@@ -0,0 +1,42 @@
+/*
+* File: member.component.ts
+* Author: Szivák Gergő
+* Copyright: 2022, Szivák Gergő
+* Group: Szoft II/N
+* Date: 2022-03-31
+* Github: https://github.com/Gergosz-2000/member
+* Licenc: GNU GPL 
+*/
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-member',
+  templateUrl: './member.component.html',
+  styleUrls: ['./member.component.css']
+})
+export class MemberComponent implements OnInit {
+
+  memberForm = new FormGroup( {
+    name: new FormControl('',Validators.required),
+    city: new FormControl('',Validators.required),
+    address: new FormControl('',Validators.required),
+    email: new FormControl('',[Validators.required,Validators.email])
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  saveMember() {
+    alert(
+      'Név: ' + this.memberForm.value.name + '\n' + 
+      'Település:' + this.memberForm.value.city + '\n' +
+      'Cím:' + this.memberForm.value.address + '\n' +
+      'E-mail:' + this.memberForm.value.email
+
+      );
+  }
+
+}
